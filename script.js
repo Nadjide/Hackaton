cb85fe228585a2d688ad159acef938db23d2af43
ludogernay <ludovic.gernay@ynov.com>
2024-01-31 10:22:31 +0100

    [+] version finale

--- script.js
+++ script.js
@@ -18,7 +18,15 @@ const num4 = document.querySelector('.num4');
 const num5 = document.querySelector('.num5');
 const info = document.querySelector('.info');
 const code = 'etlalumierefut';
-var cpt = 0;
+
+// Utilisez un objet pour stocker les états des drapeaux
+const flags = {
+    flag1: false,
+    flag2: false,
+    flag3: false,
+    flag4: false,
+    flag5: false
+};
 
 var minutes = 15;
 var secondes = 0;
@@ -50,16 +58,20 @@ function startTimer() {
 // Call startTimer() when the start button is clicked
 const startButton = document.querySelector('.start');
 startButton.addEventListener('click', startTimer);
+
 function win() {
-    if (cpt == 5) {
+    if (flags.flag1 && flags.flag2 && flags.flag3 && flags.flag4 && flags.flag5) {
         const final = document.querySelector('.final');
         const finalinput = document.querySelector('.finalinput');
         const finalbtn = document.querySelector('.finalbtn');
         final.style.display = 'block';
         finalbtn.addEventListener('click', function () {
             if (finalinput.value == code) {
+                //je veux faire une transition pour que l'overlay disparaisse lenntement
+                overlay.style.transition = 'all 4s';
                 overlay.style.opacity = '0';
-                final.innerHTML = 'Bravo ! Vous avez réussi ! <br> La salle s\' est allumée !';
+                final.style.fontSize = '50px';
+                final.innerHTML = 'Bravo ! <br> Vous avez réussi ! <br> La salle s\'est allumée !';
             }
         });
     }
@@ -76,7 +88,8 @@ document.addEventListener('mousemove', function (e) {
         overlay.style.background = `radial-gradient(circle at ${e.clientX}px ${e.clientY}px, transparent 50px, #000 100px)`;
     }
 });
-function openInfo(message, numToUpdate, btn, falseBtn, numpart) {
+
+function openInfo(message, numToUpdate, btn, falseBtn, numpart, flag) {
     open = true;
     info.style.display = 'block';
     info.style.cursor = 'default';
@@ -85,43 +98,44 @@ function openInfo(message, numToUpdate, btn, falseBtn, numpart) {
     const exit = document.querySelector('.exit');
     const input = document.querySelector('.ipt');
     const submit = document.querySelector('.valid');
-    
+
     exit.addEventListener('click', function (event) {
         indice.innerHTML = '';
         closeInfo();
     });
 
     submit.addEventListener('click', function () {
-        if (input.value == numpart) {
+        if (input.value === numpart) {
             numToUpdate.innerHTML = numpart;
             btn.style.display = 'none';
             falseBtn.style.display = 'none';
-            cpt++;
             win();
+            // Modifiez l'état du drapeau dans l'objet flags
+            flags[flag] = true;
             indice.innerHTML = '';
             closeInfo();
         } else {
-            openInfo('Mauvaise réponse !<br>', numToUpdate, btn, falseBtn, numpart);
+            openInfo('Mauvaise réponse !<br>', numToUpdate, btn, falseBtn, numpart, flag);
         }
     });
 }
 
 btn1.addEventListener('click', function () {
-    openInfo('Munissez-vous de votre boussole !<br>', num5, btn1, false1, '13');
+    openInfo('Munissez-vous de votre boussole !<br>', num5, btn1, false1, '13', 'flag1');
 });
 
 btn2.addEventListener('click', function () {
-    openInfo('Regarder derrière le tableau !<br>', num4, btn2, false2, '67');
+    openInfo('Regarder derrière le tableau !<br>', num4, btn2, false2, '67', 'flag2');
 });
 
 btn3.addEventListener('click', function () {
-    openInfo('Vous ne trouvez pas qu\'il fait chaud ?<br>', num1, btn3, false3, '06');
+    openInfo('Vous ne trouvez pas qu\'il fait chaud ?<br>', num1, btn3, false3, '06', 'flag3');
 });
 
 btn4.addEventListener('click', function () {
-    openInfo('Trouvez le bon ordi dans le bon sac !<br>', num2, btn4, false4, '34');
+    openInfo('Trouvez le bon ordi dans le bon sac !<br>', num2, btn4, false4, '34', 'flag4');
 });
 
 btn5.addEventListener('click', function () {
-    openInfo('Regardez bien toute la table !<br>', num3, btn5, false5, '78');
+    openInfo('Regardez bien toute la table !<br>', num3, btn5, false5, '78', 'flag5');
 });
